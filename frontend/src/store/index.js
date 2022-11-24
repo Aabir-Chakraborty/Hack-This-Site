e9095dbbeb037097644f6b1bd0505a629472e56e
Harshit Pandit <harshit7757@gmail.com>
2022-11-25 01:37:15 +0530

    Editing routes for questions

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 
 import authReducer from "./authSlice";
+import roundReducer from "./roundSlice";
 
 const store = configureStore({
     reducer: {
         auth: authReducer,
+        round: roundReducer,
     },
 });
 
